15a984e81e9cdea5086570fe727fff821a9a7640
daniel <drs5ma@virginia.edu>
2016-05-14 20:30:14 -0400

    added evrrrythinghign

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk(process.env.MONGODB_URI);
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,3 +72,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
